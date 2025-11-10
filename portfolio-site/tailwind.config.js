a16f331506e01d11cd374e8248a2c49365957126
mathias <mathiasmeeus2@gmail.com>
2025-11-10 13:37:00 +0100

    chore: small update and improved package security

--- portfolio-site/tailwind.config.js
+++ portfolio-site/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}', // Include all template files
+        './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}',
     ],
     theme: {
         extend: {
